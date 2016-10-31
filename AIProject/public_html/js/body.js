2634e1678c715cfcedb348ef175965d81dbb61c7
Ernesto <esaco02@gmail.com>
2016-10-30 22:37:23 -0600

    Cambios
    Agreados iconos de redes sociales
    Agredado el footer

--- AIProject/public_html/js/body.js
+++ AIProject/public_html/js/body.js
@@ -8,15 +8,14 @@ function Gsitesearch(curobj){ curobj.q.value='site:'+domainroot+' '+curobj.qfron
 function buildBody(myCallback) {
     var theContainer = $(".container");
     var header = "<div id='header' class='row'></div>";
-    var pageTitle = "<div class='col-md-6'><h1><a class='texto-blanco' href='#' id='tittle'>Hexagonal Final - RUSIA 2018</a></h1></div>";
+    var pageTitle = "<div class='col-md-8'><h1><a class='texto-blanco' href='#' id='tittle'>Hexagonal Final - RUSIA 2018</a></h1></div>";
 
-	var googleSearch="<form class='search navbar-form navbar-right' action='http://www.google.com/search' method='get'role='search' onSubmit='Gsitesearch(this)'> <div class='input-group'> <input name='q' type='hidden' /> <input class='form-control' name='qfront' type='search' required class='searchField' placeholder='Google Site Search' maxlength='50'/> <span class='input-group-btn'> <button type='submit' class='search-button btn btn-primary'> <span class='glyphicon glyphicon-search'></span> </button> </span> </div> </form>";
+	//var googleSearch="<form class='search navbar-form navbar-right' action='http://www.google.com/search' method='get'role='search' onSubmit='Gsitesearch(this)'> <div class='input-group'> <input name='q' type='hidden' /> <input class='form-control' name='qfront' type='search' required class='searchField' placeholder='Google Site Search' maxlength='50'/> <span class='input-group-btn'> <button type='submit' class='search-button btn btn-primary'> <span class='glyphicon glyphicon-search'></span> </button> </span> </div> </form>";
     var countryImage = "<div class='col-md-12'><img class='pull-right' id='countryImage' src='images/crc.png' alt='country'></div>";
 
-	var loginButton = "<div id = 'loginButton' class='col-md-3 col-sm-12 main-icons'><a class='texto-blanco' href='#'>Inicio de sesión <i class='glyphicon glyphicon-user'></i></a></div>"+
-                             "<div id = 'registerButton' class='col-md-3 col-sm-12 main-icons'><a class='texto-blanco' href='#'>Regístrese <i class='glyphicon glyphicon-pencil'></i></a></div>"+
- 							"<div class='col-md-6 col-sm-12'>"+googleSearch+"</div>";
-     var divLoginAndCountryImage = "<div class='col-md-6 pull-right' id='divLoginAndCountryImage'><div class='row'>"+loginButton+"</div><div class='row'>"+countryImage+"</div></div>";
+	var loginButton = "<div id = 'loginButton' class='col-md-6 col-sm-12 main-icons'><a class='texto-blanco' href='#'>Inicio de sesión <i class='glyphicon glyphicon-user'></i></a></div>"+
+                             "<div id = 'registerButton' class='col-md-6 col-sm-12 main-icons'><a class='texto-blanco' href='#'>Regístrese <i class='glyphicon glyphicon-pencil'></i></a></div>";
+     var divLoginAndCountryImage = "<div class='col-md-4 pull-right' id='divLoginAndCountryImage'><div class='row'>"+loginButton+"</div><div class='row'>"+countryImage+"</div></div>";
 	
 	
     theContainer.append(header);
@@ -44,7 +43,7 @@ function buildBody(myCallback) {
     $("#modalsContainer").append(modalRegister());
     
     /*Este corre el startGoogleSearch()*/
-    myCallback;
+  //  myCallback;
     $(".gsc-control-cse-es").css("background-color","transparent")
                             .css("border-color","transparent"); 
 }
